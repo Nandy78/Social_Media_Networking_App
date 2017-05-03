de8c5b91765a7b2c779e94f58514b43634c34a14
Ninad <ninadsingh7895@gmail.com>
2017-05-04 01:32:47 +0530

    Finish useredit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 
-	def log_in
+	def log_in(user)
 		session[:user_id] = user.id
 	end
 
@@ -35,13 +35,24 @@ module SessionsHelper
 	end
 
 
-
 	def log_out
 		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
 	end
 
+	def correct_user?(user)
+		user == current_user
+	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	def store_location
+		session[:forwarding_url] = request.original_url if request.get?
+	end
 
 
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,7 +19,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", user_path(@user)
 		delete logout_path
 		assert_not is_logged_in?
-		assert_redirected_root_url
+		assert_redirected_to root_url
 		delete logout_path
 		follow_redirect!
 		assert_select "a[href=?]", login_path
